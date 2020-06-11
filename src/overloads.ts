858ed90b5c0a652cf1ba8cfefcb4d74d4df6aed4
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-06-11 02:40:34 -0700

    overloads

---
+++ src/overloads.ts
@@ -0,0 +1,15 @@
+export {};
+
+function double(value: number): number;
+function double(value: string): string;
+
+function double(value: any): any {
+  if (typeof value === 'number') {
+    return value * 2;
+  } else {
+    return value + value;
+  }
+}
+
+console.log(double(100));
+console.log(double('Go '));
