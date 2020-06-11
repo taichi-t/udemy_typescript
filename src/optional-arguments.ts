c859cf368c0d7981d3404329cd295f23aa6f7f7f
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-06-11 01:59:58 -0700

    optional-arguments

---
+++ src/optional-arguments.ts
@@ -0,0 +1,14 @@
+export {};
+let bmi: (height: number, weight: number, printable?: boolean) => number = (
+  height: number,
+  weight: number,
+  printable?: boolean
+): number => {
+  const bmi: number = weight / (height * height);
+  if (printable) {
+    console.log({ bmi });
+  }
+  return bmi;
+};
+
+bmi(1.7, 90);
