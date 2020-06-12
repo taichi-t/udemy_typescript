01bee6b9be7f218d01a3bbe4cb74889b3bdcc0da
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-06-12 16:27:24 -0700

    return-interfaces

---
+++ src/returen-interfaces.ts
@@ -0,0 +1,27 @@
+export {};
+
+class Mahotsukai {}
+class Souryo {}
+
+class Taro extends Mahotsukai {}
+
+interface Kenja {
+  ionazun(): void;
+}
+
+interface Senshi {
+  kougeki(): void;
+}
+
+class Jiro implements Kenja, Senshi {
+  ionazun(): void {
+    console.log('ionazun');
+  }
+  kougeki(): void {
+    console.log('kougeki');
+  }
+}
+
+const jiro = new Jiro();
+jiro.ionazun();
+jiro.kougeki();
